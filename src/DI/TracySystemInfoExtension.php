4fb2577c57e40564a6716ecb83510d48e32e8c1d
Tomáš Kudělka <thorewi@gmail.com>
2022-02-05 14:37:17 +0100

    Update TracySystemInfoExtension.php

--- src/DI/TracySystemInfoExtension.php
+++ src/DI/TracySystemInfoExtension.php
@@ -3,14 +3,14 @@
 namespace ADT\TracySystemInfo\DI;
 
 
-use ADT\TracySystemInfo\SetterCommand;
 use ADT\TracySystemInfo\Storage;
 use Nette;
 use Nette\DI\Extensions\InjectExtension;
 
-class TracySystemInfoExtension extends \Nette\DI\CompilerExtension {
-
-	public function loadConfiguration() {
+class TracySystemInfoExtension extends \Nette\DI\CompilerExtension 
+{
+	public function loadConfiguration() 
+	{
 		$config = $this->getConfig();
 
 		if (empty($config['storageFile'])) {
@@ -21,18 +21,11 @@ class TracySystemInfoExtension extends \Nette\DI\CompilerExtension {
 			->addDefinition($this->prefix('storage'))
 			->setFactory(Storage::class)
 			->setArguments([ $config['storageFile'] ]);
-
-		$this->getContainerBuilder()
-			->addDefinition($this->prefix('hostnameSetterCommand'))
-			->setFactory(SetterCommand::class)
-			->addTag(InjectExtension::TAG_INJECT, FALSE)
-			->addTag('console.command');
 	}
 
-	public function afterCompile(Nette\PhpGenerator\ClassType $class) {
+	public function afterCompile(Nette\PhpGenerator\ClassType $class) 
+	{
 		$initMethod = $class->methods['initialize'];
 		$initMethod->addBody('$_data = &$this->getService(?)->getPanel(?)->data; $_data = $_data \?: []; if (is_array($_data)) { $_data += $this->getService(?)->get();}', [ 'tracy.bar', 'Tracy:info', $this->prefix('storage') ]);
 	}
-
-
 }
